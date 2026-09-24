f52cbf96c979fb396cf6e3f3ba7d72249a0778a0
akash1723tripathi <akashtripathi.acad@gmail.com>
2026-09-24 21:08:28 +0530

    feat(client): add placeholder LoginPage component

---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const LoginPage: React.FC = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">LoginPage</h1>
+    </div>
+  );
+};
